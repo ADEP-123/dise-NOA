7929f3322281bb53d7698da2940fd39e3654de9e
ADEP-123 <andep210399@gmail.com>
2024-04-21 17:54:59 -0500

    feat: :sparkles: zona del catalogo menu de crear y modificar producto

--- views/inventario/scripts/inventario.js
+++ views/inventario/scripts/inventario.js
@@ -1,7 +1,34 @@
 const catalButton = document.querySelector(".catalButton");
 
-catalButton.addEventListener("click",e=>{
+catalButton.addEventListener("click", e => {
     e.preventDefault();
     e.stopPropagation();
-    
+
+})
+
+// catalogo
+const openModMenuCat = document.querySelector(".modorNewObjectButt");
+const catalInfo = document.querySelector(".catalInfo");
+let ispOpenCat = false;
+openModMenuCat.addEventListener("click", e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const menu = openModMenuCat.parentNode;
+    const modObjectSide = document.querySelector(".modOrNewObject");
+    if (ispOpenCat == false) {
+        menu.style.width = "20%"
+        catalInfo.style.width = "80%"
+        openModMenuCat.style.width = "10%"
+        openModMenuCat.innerHTML = "→"
+        modObjectSide.style.display = "flex"
+        ispOpenCat = true
+    } else {
+        menu.style.width = "2%"
+        catalInfo.style.width = "98%"
+        openModMenuCat.style.width = "100%"
+        openModMenuCat.innerHTML = "←"
+        modObjectSide.style.display = "none"
+        ispOpenCat = false
+    }
+
 })
