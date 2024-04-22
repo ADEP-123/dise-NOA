dbdc930eb83dde6fa9e1d27e4c0e01b0efd63114
ADEP-123 <andep210399@gmail.com>
2024-04-21 20:10:40 -0500

    fix: :bug: llenar selects reparado

--- views/inventario/scripts/llenarSelect.js
+++ views/inventario/scripts/llenarSelect.js
@@ -1,14 +1,20 @@
 const llenarSelect = (products) => {
     const selectProduct = document.querySelector("#searcProdSelect");
     const selectModProduct = document.querySelector("#modProdSelect");
+    console.log({ selectProduct, selectModProduct });
+    selectProduct.innerHTML = "";
+    selectModProduct.innerHTML = "";
 
     products.forEach(element => {
         const newOption = document.createElement("option");
+        const newOption2 = document.createElement("option");
         newOption.value = element.id;
-        newOption.innerHTML = `${element.titulo}`
+        newOption.innerHTML = `${element.id}-${element.titulo}`
+        newOption2.value = element.id;
+        newOption2.innerHTML = `${element.id}-${element.titulo}`
 
         selectModProduct.insertAdjacentElement("beforeend", newOption);
-        selectProduct.insertAdjacentElement("beforeend", newOption);
+        selectProduct.insertAdjacentElement("beforeend", newOption2);
     });
 }
 export default llenarSelect
